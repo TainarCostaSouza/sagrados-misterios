703d667385320a5405f10df90dcd1c7ad40aa04d
TainarCostaSouza <tainarcostasouza@gmail.com>
2025-04-18 22:18:28 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+
+document.getElementById("root").innerHTML = `
+  <h1 style='text-align:center;'>🌌 Sagrados Mistérios 🌌</h1>
+  <p style='text-align:center;'>Este é o início do seu site cabalístico. Em breve, funcionalidades como mapa astral e gematria estarão aqui.</p>
+`;
